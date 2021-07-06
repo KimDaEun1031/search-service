aae2af89c34c1a0a067e6c4791666508ca8293df
KimDaEun1031 <kimde1031@gmail.com>
2021-07-06 22:29:55 +0900

    Initial commit

---
+++ src/main/java/org/daeun/msascreen/MsascreenApplication.java
@@ -0,0 +1,13 @@
+package org.daeun.msascreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsascreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsascreenApplication.class, args);
+	}
+
+}
